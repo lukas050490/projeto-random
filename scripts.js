5fc9838ea697b231a3defec31396f706c8ab595b
lukas050490 <lucas_alexandre05@hotmail.com>
2024-02-09 16:32:05 -0300

    meu projeto random

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function generateNumber(){
+const min = math.ceil(document.querySelector(".input-min").Value)
+
+const max = math.floor(document.querySelector(".input-max").Value)
+
+const result = Math.floor(Math.random() * (máx - min + 1)) + min;
+
+alert(" result")
+}
